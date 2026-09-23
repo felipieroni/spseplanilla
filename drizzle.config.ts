6453a69d5e18a4632f5bf8b494fbbcce1e7b8946
felipieroni <felipepieroni2008@gmail.com>
2026-09-23 09:04:48 -0300

    Preparado para deploy

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'drizzle-kit';
+import { config } from 'dotenv';
+
+config({ path: '.env.local' });
 
 export default defineConfig({
-  dialect: "sqlite",
+  dialect: 'turso',
   schema: './src/lib/db/schema.ts',
   out: './drizzle',
   dbCredentials: {
-    url: './sqlite.db',
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
   },
 });
